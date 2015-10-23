80eebc853c1c1ee46c023a578d14590e838b8b88
Mina Mikhail <mina@fightingtheboss.com>
2015-10-23 16:04:32 -0400

    Create Todo model, populate view with data from DB

--- todo.rb
+++ todo.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 require 'data_mapper'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/todos')
+
+class Todo
+  include DataMapper::Resource
+
+  property :id,           Serial    # An auto-incremented unique integer key
+  property :task,         String    # A short string of characters
+  property :completed_at, DateTime  # Stores the date and time that the task was completed
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
 get '/' do
-  @todos = ['Build a time machine', 'Go back in time', 'Meet parents', 'Convince them to fall in love', 'Go Back to the Future']
+  @todos = Todo.all
   erb :index
 end
